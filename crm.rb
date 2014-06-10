645e9a0df40246b4ff91f50ce11e3b5d768179a2
Josh Cornelius <joshcornelius9@gmail.com>
2014-06-10 12:51:37 -0400

    completed exercise

--- crm.rb
+++ crm.rb
@@ -15,8 +15,15 @@ get "/contacts" do
   erb :contacts
 end
 
-# get '/contacts/new' do
-# end
+post "/contacts" do
+	new_contact = Contact.new(params[:first_name],params[:last_name],params[:email], params[:note])
+	@@rolodex.add_contact(new_contact)
+	redirect to('/contacts')
+end
+
+get '/contacts/new' do
+	erb :new_contact
+end
 
 # get '/contacts/:id/edit' do
 # end
